d1905c55de89a53e3363d84bc1a378a6136cb49c
Danh Ho <dmho@ucsd.edu>
2021-02-11 16:13:55 -0800

    event endpoint

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,39 +4,60 @@ class EventsController < ApplicationController
   # GET /events
   def index
     @events = Event.all
-
+    
     render json: @events
   end
 
-  # GET /events/1
-  def show
-    render json: @event
-  end
+  # # GET /events/1
+  # def show
+  #   render json: @event
+  # end
 
   # POST /events
   def create
     @event = Event.new(event_params)
+    @event.save
 
-    if @event.save
-      render json: @event, status: :created, location: @event
+
+    
+      # msg = {:status => "ok", :message => "sucess"}
+      # msg = {'message' => 'Saved Successful'}
+      # render :json => msg
+      # render json: {status: :created}
+
+    if @event[:name] != nil && @event[:event_type] != nil
+      if @event[:event_type] != "click" || @event[:event_type] != "view" || @event[:event_type] != "edit"
+        @event.errors.add(:event_type, "#{@event.event_type} is not a valid event type")
+        render json: @event.errors, status: :unprocessable_entity
+      else 
+        render json: @event, status: :created, location: @event
+      end
     else
+      if @event[:event_type] == nil
+        @event.errors.add(:event_type, "must be given")
+      else
+      end
+      if @event[:name] == nil
+        @event.errors.add(:name, "must be given")
+      else
+      end
       render json: @event.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /events/1
-  def update
-    if @event.update(event_params)
-      render json: @event
-    else
-      render json: @event.errors, status: :unprocessable_entity
-    end
-  end
+#   # PATCH/PUT /events/1
+#   def update
+#     if @event.update(event_params)
+#       render json: @event
+#     else
+#       render json: @event.errors, status: :unprocessable_entity
+#     end
+#   end
 
-  # DELETE /events/1
-  def destroy
-    @event.destroy
-  end
+#   # DELETE /events/1
+#   def destroy
+#     @event.destroy
+#   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -47,5 +68,6 @@ class EventsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def event_params
       params.require(:event).permit(:name, :event_type, :at, :button_color)
+      # params.require(:event).require()
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    # validates :name, presence: {message: "must be given"}
+    # validates :event_type,  presence: {message: "must be given"}, inclusion: { in: %w( click view edit), message: "%{value} is not a valid event type" }
 end
